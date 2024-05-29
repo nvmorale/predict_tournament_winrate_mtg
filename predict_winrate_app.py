251ea23901fe03f5774632a7af86c2d461c02d4d
nvmorale <nvmorale@gmail.com>
2024-05-29 00:31:37 -0400

    Default wr matrix
    Added default wr matrix explaining expected format

--- predict_winrate_app.py
+++ predict_winrate_app.py
@@ -5,6 +5,15 @@ import os
 import plotly.express as px
 import plotly.graph_objs as go
 
+DEFAULT_WR_MATRIX = pd.DataFrame({
+    "Overall": [50,49,51,40],
+    "Aggro": [50,60,40,40],
+    "Midrange": [60, 50, 40,40],
+    "Control": [40,60,50,40],
+},
+    index = ["Aggro", "Midrange", "Control", "Tier 4 deck"],
+)
+
 
 st.set_page_config(layout="wide")
 st.title("Predict Winrate MTG")
@@ -13,16 +22,31 @@ def main():
     
     st.write(f"App running with Streamlit v. {st.__version__}")
     
-    uploaded_file = st.file_uploader("Upload Excel file with winrate matrix")
-    if uploaded_file is not None:
-
-        # Can be used wherever a "file-like" object is accepted:
-        wr_matrix = pd.read_excel(uploaded_file, index_col = 0, header = 0)
+    
+    use_default_matrix = st.toggle("Use default wr matrix")
+    
+    
+    
+    if use_default_matrix:
+        st.write("""This dataframe has the required format for Excel file \r\n
+Rows are the decks that you want to evaluate \r\n
+Columns are the meta decks that you have info about its meta share \r\n
+Overall column is required for each deck""")
+        wr_matrix = DEFAULT_WR_MATRIX
+    else:
+        wr_matrix = None
+        uploaded_file = st.file_uploader("Upload Excel file with winrate matrix")
+        if uploaded_file is not None:
+            # Can be used wherever a "file-like" object is accepted:
+            wr_matrix = pd.read_excel(uploaded_file, index_col = 0, header = 0)
+    
+    if wr_matrix is not None:
+            
         st.write(wr_matrix)
-        
+
         meta_decks = list(wr_matrix.columns)[1:]
         meta_predictions = {}
-        
+
         with st.sidebar:
             st.write("Fill your metagame prediction (0 to 100)")
             for deck in meta_decks:
@@ -34,16 +58,16 @@ def main():
 
             if others_meta < 0:
                 st.write("Corrrect meta predictions to sum less than 100%")
-                
+
             meta_vector = np.array([others_meta] + list(meta_predictions.values()))
-            
+
         if meta_vector is not None:
             expected_wr = wr_matrix.dot(meta_vector)/100
             expected_wr.columns = ["Winrate"]
-            
+
             st.write("Expected Winrate per deck")
             st.dataframe(expected_wr, use_container_width=True)
-    
+
 #####    
     
 if __name__ == '__main__':
